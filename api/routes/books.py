eab203fc3fb7bdfc3c377bb693986aae06154f17
Umario <umorinoabdul@gmail.com>
2025-02-12 20:17:50 +0100

    Add AmazingFeature

--- api/routes/books.py
+++ api/routes/books.py
@@ -48,6 +48,21 @@ async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
 
 
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book(book_id: int):
+    book = db.get_book(book_id)
+    if book == None:
+        return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content={"detail": "Book not found"},
+                )
+
+    return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=book.model_dump(),
+            )
+
+
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def update_book(book_id: int, book: Book) -> Book:
     return JSONResponse(
